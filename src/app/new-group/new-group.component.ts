e456e4cfd582e8b413c2ddf0bae3ace548759143
Mazhitov Keneshbek <majitovkesha@gmail.com>
2021-11-23 12:57:12 +0600

    add form for create new group and add to groups

--- src/app/new-group/new-group.component.ts
+++ src/app/new-group/new-group.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { GroupsService } from '../shared/Groups.service';
+import { Group } from '../shared/Group.model';
 
 @Component({
   selector: 'app-new-group',
   templateUrl: './new-group.component.html',
   styleUrls: ['./new-group.component.css']
 })
-export class NewGroupComponent implements OnInit {
+export class NewGroupComponent {
+  @ViewChild('groupNameInput') groupName!:ElementRef;
+  constructor(private groupsService: GroupsService) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  onAddGroup() {
+    const groupName = this.groupName.nativeElement.value;
+    const newGroup = new Group(groupName, []);
+    this.groupsService.addGroup(newGroup);
+    this.reset();
   }
 
+  reset() {
+    this.groupName.nativeElement.value = '';
+  }
 }
