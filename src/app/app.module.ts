8e1d4e48a78eda18a7b407847f0bc3d8b0ee9a9c
Mazhitov Keneshbek <majitovkesha@gmail.com>
2021-11-23 11:11:24 +0600

    create component app-users

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { NewUserComponent } from './new-user/new-user.component';
+import { UsersComponent } from './users/users.component';
+import { UserComponent } from './users/user/user.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NewUserComponent
+    NewUserComponent,
+    UsersComponent,
+    UserComponent
   ],
   imports: [
     BrowserModule
